efc40dcf6a26e738492af7b96b9ce0ab72d0564b
Aubloayjawan711 <fahemgawan@gmail.com>
2025-08-01 23:57:02 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,27 @@
-
 self.addEventListener('install', event => {
-    console.log('[Service Workwe] Installing...');
-  event.waitUntil( 
-
-    caches.open('paw-v1').then(cache => {
+  console.log('[Service Worker] Installing...');
+  event.waitUntil(
+    caches.open('pwa-v1').then(cache => {
       return cache.addAll([
-        './index.html',
-        './style.css',
-        './manifest.json',
-        './notify.js',
-        './icon-192x192.png',
-        './icon-512x512.png'
-
+        '/push_Notification/index.html',
+        '/push_Notification/style.css',
+        '/push_Notification/manifest.json',
+        '/push_Notification/icons/icon-192x192.png',
+        '/push_Notification/icons/icon-512x512.png'
       ]);
     })
   );
 });
- 
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  );
 });
 
 self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data?.url || '/')
-    );
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data?.url || '/push_Notification/')
+  );
 });
-
-
